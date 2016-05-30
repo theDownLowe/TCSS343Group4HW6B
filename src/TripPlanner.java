cf2667fc861ab32e729bf3d1f8f5f17a64b9688a
theDownLowe <tnlral@uw.edu>
2016-05-29 19:07:32 -0700

    Driver 95% complete

--- src/TripPlanner.java
+++ src/TripPlanner.java
@@ -21,42 +21,87 @@ public class TripPlanner {
 		System.out.println("----- Trip Planner -----");
 		SimpleGraph tripGraph = new SimpleGraph();
 		Hashtable<String, Vertex> graphTable = GraphInput.LoadSimpleGraph(tripGraph); // Name, Vertex
-		userMenu(tripGraph);
+		userMenu(tripGraph, graphTable);
 	}
 	
-	public static void userMenu(SimpleGraph tripGraph) {
+	public static void userMenu(SimpleGraph tripGraph, Hashtable<String, Vertex> graphTable) {
 		int selection = 0;
-		while (selection != 4) {
+		while (selection != 3) {
 			System.out.println("----- Main Menu -----");
-			System.out.println("1) Run Dijkstra's Algorithm (Heap)");
-			System.out.println("2) Run Dijkstra's Algorithm (Array)");
-			System.out.println("3) List cities");
-			System.out.println("4) Exit");
+			System.out.println("1) Find best route");
+			System.out.println("2) List cities");
+			System.out.println("3) Exit");
+			System.out.print("Selection: ");
 			
 			Scanner input = new Scanner(System.in);
 			selection = input.nextInt();
 			if (selection == 1) { 			// Heap
-				runDijkstrasHeap(tripGraph);
-			} else if (selection == 2) { 	// Array
-				runDijkstrasArray(tripGraph);
-			} else if (selection == 3) { 	// List Cities
+				findRoute(tripGraph, graphTable, input);
+			} else if (selection == 2) { 	// List Cities
 				listCities(tripGraph);
-			} else if (selection != 4) {	// Invalid
+			} else if (selection != 3) {	// Invalid
 				System.out.println("Please enter a valid input.");
 			}
 			
 		}
 	}
 	
-	private static void runDijkstrasHeap(SimpleGraph tripGraph) {
+	public static void findRoute(SimpleGraph tripGraph, Hashtable<String, Vertex> graphTable, Scanner input) {
+		System.out.println("----- Find Route -----");
+		boolean found = false;
+		Vertex start = null;
+		Vertex end = null;
+		
+		while (!found) {
+			System.out.print("Enter a starting location: ");
+			String s = input.next();
+			if (graphTable.containsKey(s)) {
+				start = graphTable.get(s);
+				found = true;
+			} else {
+				System.out.println("Please enter a valid city name (case sensitive).");
+			}
+		}
+		
+		found = false;
+		while (!found) {
+			System.out.print("Enter an end location: ");
+			String s = input.next();
+			if (graphTable.containsKey(s)) {
+				end = graphTable.get(s);
+				found = true;
+			} else {
+				System.out.println("Please enter a valid city name (case sensitive).");
+			}
+		}
+		
+		while (!found) {
+			System.out.println("Which version of Dijkstra's algorithm would you like to use?");
+			System.out.println("1) Heap version");
+			System.out.println("2) Array version"); 
+			System.out.print("Selection: ");
+			
+			int selection = input.nextInt();
+			if (selection == 1) {
+				runDijkstrasHeap(tripGraph, start, end);
+			} else if (selection == 2) {
+				runDijkstrasArray(tripGraph, start, end);
+			} else {
+				System.out.println("Please enter a valid input.");
+			}
+		}
+	}
+	
+	private static void runDijkstrasHeap(SimpleGraph tripGraph, Vertex start, Vertex end) {
 		
 	}
 	
-	private static void runDijkstrasArray(SimpleGraph tripGraph) {
+	private static void runDijkstrasArray(SimpleGraph tripGraph, Vertex start, Vertex end) {
 		
 	}
 	
 	private static void listCities(SimpleGraph tripGraph) {
+		System.out.println("----- List Cities -----");
 		for (Object v: tripGraph.vertexList.toArray()) {
 			Vertex vert = (Vertex) v;
 			System.out.println(vert.getName());
